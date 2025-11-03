fd832fcdd60a0acb20cf070e1879d90b065728b1
project <2503454@upv.edu.mx>
2025-11-03 10:40:24 -0600

    feat: understanding_numbers

---
+++ src/understanding_numbers.py
@@ -0,0 +1,33 @@
+# Numbers
+"""
+Enteros
+    Los podemos sumar (+), restar(-), multiplicar(*)
+    y dividir (/)
+    Les podemos potenciar (**,**3,**4)
+
+    """
+
+number_1 = 35
+number_2 = 15
+suma = number_1 + number_2
+resta = number_1-number_2
+multiplicacion = number_1*number_2
+division = number_1/number_2
+potencia = number_1**2
+modulo = number_1%number_2
+print("suma: ", suma, type(suma))
+print("resta: ", resta, type(resta))
+print("multiplicacion: ", multiplicacion, type(multiplicacion))
+print("division: ", division, type(division))
+print("potencia", potencia, type(potencia))
+print("modulo", modulo, type(modulo))
+
+"""
+jerarquia de operaciones
+"""
+
+"""
+floats
+    python llama floats a cualquier numero con punto decimal
+
+    
